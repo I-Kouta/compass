08a8b2f8159cc9b3d012ebe33ea0c3644a01eb35
I-Kouta <kouta.baseball0222@gmail.com>
2022-12-17 13:26:28 +0900

    RegisterFormRequestの使うための記述

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -178,6 +178,7 @@
         </div>
         <div class="mt-5 text-right">
           <input type="submit" class="btn btn-primary register_btn" disabled value="新規登録" onclick="return confirm('登録してよろしいですか？')">
+          <!-- registerコントローラー・registerPostメソッドへ -->
         </div>
         <div class="text-center">
           <a href="{{ route('loginView') }}">ログイン</a>
