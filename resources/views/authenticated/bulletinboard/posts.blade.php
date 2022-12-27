295e1a59f80de1d4afe494ef4b8b707c4afd8a85
I-Kouta <kouta.baseball0222@gmail.com>
2022-12-27 14:33:01 +0900

    メモ追記です

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -17,6 +17,7 @@
             @if(Auth::user()->is_Like($post->id))
             <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}"></span></p>
             <!-- iクラスはハート本体、span like_countsは数字 -->
+            <!-- post_id,classは投稿の区別の命名 -->
             @else
             <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}"></span></p>
             @endif
