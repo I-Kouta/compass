5db701bd46341689ff3e1c6c1c0e624882bee706
I-Kouta <kouta.baseball0222@gmail.com>
2022-12-29 14:43:27 +0900

    メイン・サブカテゴリーのリレーションの定義

--- app/Models/Categories/MainCategory.php
+++ app/Models/Categories/MainCategory.php
@@ -13,6 +13,6 @@ class MainCategory extends Model
     ];
 
     public function subCategories(){
-        // リレーションの定義
+        return $this->hasMany('App\Models\Categories\SubCategory');
     }
 }
