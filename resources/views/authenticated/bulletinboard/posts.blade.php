626a94a184ae815affe8f549cadaeb116a9e067d
I-Kouta <kouta.baseball0222@gmail.com>
2023-01-01 13:05:40 +0900

    コメントアイコンにリンクをつける記述

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -11,7 +11,9 @@
       <div class="post_bottom_area d-flex">
         <div class="d-flex post_status">
           <div class="mr-5">
-            <i class="fa fa-comment"></i><span class=""></span>
+            <a href="{{ route('post.detail', ['id' => $post->id]) }}">
+              <i class="fa fa-comment"></i><span class=""></span>
+            </a>
           </div>
           <div>
             @if(Auth::user()->is_Like($post->id))
