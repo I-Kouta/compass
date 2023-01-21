9bcc72f2df34466b1f1e67921f679b3db3dd9c2d
I-Kouta <kouta.baseball0222@gmail.com>
2023-01-21 12:02:15 +0900

    サブカテゴリ名で検索を行う記述

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -29,7 +29,10 @@ class PostsController extends Controller
             ->orWhere('post', 'like', '%'.$request->keyword.'%')->get();
         }else if($request->category_word){ // カテゴリーワードが送られてきている場合
             $sub_category = $request->category_word;
-            $posts = Post::with('user', 'postComments')->get();
+            $posts = Post::with('user', 'postComments')
+            ->whereHas('subCategories', function ($q) use ($sub_category) {
+                $q->where('sub_category', '=', $sub_category);
+            })->get();
         }else if($request->like_posts){ // いいねした投稿
             $likes = Auth::user()->likePostId()->get('like_post_id');
             $posts = Post::with('user', 'postComments')
