9961cc131ce3d7f14ee3663fe1eea74fecd95564
I-Kouta <kouta.baseball0222@gmail.com>
2023-01-21 13:44:41 +0900

    検索条件欄に選択科目を表示させる記述

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -91,6 +91,12 @@
           </div>
           <div class="selected_engineer">
             <label>選択科目</label>
+            @foreach($subjects as $subject)
+            <div class="">
+              <label>{{ $subject->subject }}</label>
+              <input type="checkbox" name="subject[]" value="{{ $subject->id }}">
+            </div>
+            @endforeach
           </div>
         </div>
       </div>
